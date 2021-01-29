1722683b88c5c2257f41e077bbc0698a8b0080fb
Ujjwal Singh Bhadoria <ujjwalsinghgravity@gmail.com>
2021-01-30 00:11:38 +0530

    Minimal Changes

--- src/main/java/JsonReader.java
+++ src/main/java/JsonReader.java
@@ -1,15 +1,26 @@
 import com.github.opendevl.JFlat;
-
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.*;
 import java.math.BigInteger;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+
+// model class consisting of max id and max last updated
+// modify readJsonfunction
+// book_update to json
 public class JsonReader {
     // Data apis.
     private static BigInteger max_ID, maxLastUpdated;
@@ -17,89 +28,91 @@ public class JsonReader {
     final private static String TEST_API = "https://gist.githubusercontent.com/sharish/45aa2116865624e59bf700e275954342/raw/8e34e8c13820dd7a16164e1438016f50f073f307/test.json";
     final private static String LESSON_API = "https://gist.githubusercontent.com/sharish/2b342b3b65fcb1d69af9d4b4c165ad13/raw/d5b1e3553f231135d4025b1848298795f192b6c6/lesson.json";
     private static boolean load_more = true;
+    public static final int  MAX_API_COUNT = 10;// 50000
 
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws IOException, JSONException {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        Set<String> set = new HashSet<>();
-        set.add("test");
-        set.add("lesson");
-        set.add("subject");
+        String perlApi = "https://drive.google.com/file/d/1yNySiIDfdpeJenIbmhC3em88-kN1VpiR/view?usp=sharing";
+        String modifiedPerlApi = convertDownloadableLink(perlApi);
+//        JSONArray jsonArray = readJsonFromUrl(modifiedPerlApi);
+//        System.out.println(jsonArray.toString());
 
-        if (set.contains(input))
-            JsonToCsv(input);
-        else
-            System.out.println("Provide valid input");
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        Set<String> set = new HashSet<>();
+//        set.add("test");
+//        set.add("lesson");
+//        set.add("subject");
+//       // getJSON();
+
+//        if (set.contains(input))
+//            jsonToCsv(input);
+//        else
+//            System.out.println("Provide valid input");
     }
 
-
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
-    public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            String jsonText = readAll(rd);
-            JSONObject jsonObject = new JSONObject(jsonText);
-            jsonObject = jsonObject.getJSONObject("data");
-            load_more = (jsonObject.getBoolean("load_more"));
-
-            System.out.println(load_more);
-            return jsonObject.getJSONArray("data");
-        }
-    }
-
-    public static void JsonToCsv(String apiType) throws IOException, JSONException {
+    public static void jsonToCsv(String apiType) throws IOException, JSONException {
 
         String inputApi = validate(apiType);
-        // string array consists of desired fields //
-
         HashSet<String> wantedFields = desiredFields();
 
-        while (load_more) {
-            JSONArray jsonArray = readJsonFromUrl(inputApi);
+        int counter = MAX_API_COUNT;
+
+        while (load_more || counter>0) {
+            JSONArray jsonArray = readJsonFromUrl(inputApi, max_ID, maxLastUpdated);
             if (jsonArray == null)
                 break;
+
             String jsonDataString = partitioningJsonArray(jsonArray, wantedFields);
+            writingToJson(jsonDataString,apiType);
             writingJsonToCsv(jsonDataString, apiType);
+            counter--;
         }
     }
 
-    // bhool gae false karna tio iuska mtlab next data jasoa arry empty:: /
-    //total count break//
     public static String validate(String apiType) {
-        String inputApi = "";
-        if (apiType.equals("lesson"))
-            inputApi = LESSON_API;
-        else if (apiType.equals("test"))
-            inputApi = TEST_API;
-        else if (apiType.equals("subject"))
-            inputApi = SUBJECT_API;
-        return inputApi;
+        return switch (apiType) {
+            case "lesson" -> LESSON_API;
+            case "test" -> TEST_API;
+            case "subject" -> SUBJECT_API;
+            default -> "";
+        };
     }
 
     public static HashSet<String> desiredFields() {
         HashSet<String> wantedFields = new HashSet<>();
-        wantedFields.add("_id");
-        wantedFields.add("last_updated");
-        wantedFields.add("correct");
+        wantedFields.add("body_encrypt");
         wantedFields.add("subject_id");
         return wantedFields;
     }
 
-    // required fileds present
-    // false msg break;;
+    //first 0,0
+
+    public static JSONArray readJsonFromUrl(String inputApi, BigInteger maxID, BigInteger maxLastUpdated) throws IOException {
+        HttpGet request = new HttpGet(inputApi);
+
+        setHeaderToRequest(request);
+        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpResponse response = client.execute(request);
+
+        HttpEntity entity = response.getEntity();
+        String result = EntityUtils.toString(entity);
+        JSONObject jsonObject = new JSONObject(result);
+        JSONObject jo1 = jsonObject.getJSONObject("data");
+
+        jsonObject = jsonObject.getJSONObject("data");
+        load_more = (jsonObject.getBoolean("load_more"));
+
+        return jo1.getJSONArray("data");
+    }
+
+
+    // maxid, maxLast, json
 
     public static String partitioningJsonArray(JSONArray jsonArray, HashSet<String> wantedFields) {
         max_ID = new BigInteger("1");
         maxLastUpdated = new BigInteger("1");
+
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject k = (JSONObject) jsonArray.get(i);
             ArrayList<String> arr = new ArrayList<>(k.keySet());
@@ -112,8 +125,18 @@ public class JsonReader {
             max_ID = max_ID.max(tempMaxId);
             maxLastUpdated = maxLastUpdated.max(tempLastUpdated);
         }
-        System.out.println("MAX BIGINT: " + max_ID);
+      //  System.out.println("MAX BIGINT: " + max_ID);
         return jsonArray.toString();
+    }
+
+    public static void writingToJson(String jsonArray,String apiType) {
+        try {
+            FileWriter file = new FileWriter("/Users/apple/Desktop/"+apiType+".json");
+            file.write(jsonArray);
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void writingJsonToCsv(String jsonDataString, String apiType) {
@@ -128,11 +151,91 @@ public class JsonReader {
         }
     }
 
+    public static void setHeaderToRequest(HttpGet request){
+        request.addHeader("platform","android");
+        request.addHeader("timestamp","epochtime");
+        request.addHeader("app-version","200");
+    }
+
     public static BigInteger getMaxID() {
         return max_ID;
     }
 
     public static BigInteger getMaxLastUpdated() {
-        return max_ID;
+        return maxLastUpdated;
     }
+
+    // for converting google drive links into downloadable
+    public static String convertDownloadableLink(String url) {
+        String[] arr = url.split("/");
+        return "https://drive.google.com/u/0/uc?id=" + arr[5] + "&export=download";
+    }
+
+//    public static void uploadFile(){
+//        String url = "https://example.com/some_endpoint";
+//        File file = new File("/Users/apple/Desktop/lesson.csv");
+//
+//        try (CloseableHttpClient client = HttpClients.createDefault()) {
+//            HttpPost post = new HttpPost(url);
+//            HttpEntity entity = MultipartEntityBuilder.create().addPart("file", new FileBody(file)).build();
+//            post.setEntity(entity);
+//
+//            try (CloseableHttpResponse response = client.execute(post)) {
+//                // ...
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    private static void executeRequest(HttpPost httpPost) {
+        try {
+            HttpClient client =  HttpClientBuilder.create().build();;
+            HttpResponse response = client.execute(httpPost);
+            System.out.println("Response Code:  " + response.getStatusLine().getStatusCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void executeMultiPartRequest(String urlString, File file) {//
+        HttpPost postRequest = new HttpPost(urlString);
+        String accessToken = "";
+        HttpPost modifiedPostRequest = addHeader(postRequest, accessToken);
+        try {
+            modifiedPostRequest.setEntity(new FileEntity(file));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        executeRequest(modifiedPostRequest);
+    }
+
+    private static HttpPost addHeader(HttpPost httpPost, String accessToken) {
+        httpPost.setHeader("Authorization", "Bearer " + accessToken);
+        return httpPost;
+    }
+
+    /*
+    public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(rd);
+            JSONObject jsonObject = new JSONObject(jsonText);
+            jsonObject = jsonObject.getJSONObject("data");
+            load_more = (jsonObject.getBoolean("load_more"));
+
+            System.out.println(load_more);
+            return jsonObject.getJSONArray("data");
+        }
+     }
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+*/
+
 }
--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,56 +1,82 @@
 import org.json.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSuite {
     public static final String API = "https://gist.githubusercontent.com/sharish/2b342b3b65fcb1d69af9d4b4c165ad13/raw/d5b1e3553f231135d4025b1848298795f192b6c6/lesson.json";
-    @Test
-    public void generate(){
-        System.out.println("This is good");
+    public static String maxId = "";
+    public static String maxLastUpdated = "";
+    public static String jsonStringFromLocal = "";
+
+
+    @BeforeEach
+    public void beforeApiCall() throws IOException, ParseException {
+        BigInteger max_id = new BigInteger("1");
+        BigInteger max_last_updated = new BigInteger("1");
+        JsonReader.jsonToCsv("lesson");
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject =  (JSONObject) jsonParser.parse(new FileReader("src/main/resources/lesson.json"));
+        String str = jsonObject.toJSONString();
+        org.json.JSONObject jsonObject1 = new org.json.JSONObject(str);
+        org.json.JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
+        JSONArray jsonArray = jsonObject2.getJSONArray("data");
+
+        jsonStringFromLocal = jsonArray.toString();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            org.json.JSONObject jsonObject3 = jsonArray.getJSONObject(i);
+            BigInteger tempId = new BigInteger(jsonObject3.getString("_id"),16);
+            BigInteger tempLastUpdated = jsonObject3.getBigInteger("last_updated");
+
+            max_id = max_id.max(tempId);
+            max_last_updated = max_last_updated.max(tempLastUpdated);
+        }
+        maxId = max_id.toString();
+        maxLastUpdated = max_last_updated.toString();
     }
+
 
     @Test
     public void validateTest(){
-        String originalApi = "https://gist.githubusercontent.com/sharish/2b342b3b65fcb1d69af9d4b4c165ad13/raw/d5b1e3553f231135d4025b1848298795f192b6c6/lesson.json";
-        boolean isApiCorrect =false;
         String inputApi = JsonReader.validate("lesson");
-        assertEquals(inputApi,originalApi);
+        assertEquals(inputApi,API);
     }
     @Test
     public void desiredFieldsTest(){
         HashSet<String> wantedFields =  new HashSet<>();
         wantedFields.add("_id");
         wantedFields.add("last_updated");
-        wantedFields.add("correct");
         wantedFields.add("subject_id");
         assertEquals(JsonReader.desiredFields(),wantedFields);
     }
 
     @Test
-    public void readJsonFromUrlTest() throws IOException, org.json.simple.parser.ParseException {
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject =  (JSONObject) jsonParser.parse(new FileReader("src/main/resources/lesson.json"));
-        String str = jsonObject.toJSONString();
-        org.json.JSONObject jsonObject1 = new org.json.JSONObject(str);
-
-        org.json.JSONObject jsonObject2 = jsonObject1.getJSONObject("data");
-        JSONArray jsonArray = jsonObject2.getJSONArray("data");
-            JSONArray expected = JsonReader.readJsonFromUrl(API);
-            String expectedJsonString = jsonArray.toString();
-            String actualJsonString = expected.toString();
-            assertEquals(expectedJsonString, actualJsonString);
+    public void readJsonFromUrlTest() throws IOException{
+//        JSONArray expectedJson = JsonReader.readJsonFromUrl(API,maxId,maxLastUpdated);
+//            String expectedJsonString = expectedJson.toString();
+//            String actualJsonString = jsonStringFromLocal;
+//            assertEquals(expectedJsonString, actualJsonString);
     }
 
     @Test
-    public void getMaximum_id(){
+    public void getMaxIdTest()  {
+        assertEquals(JsonReader.getMaxID().toString(), maxId);
+    }
 
+    @Test
+    public void getMaxLastUpdated(){
+        assertEquals(JsonReader.getMaxLastUpdated().toString(),maxLastUpdated);
     }
 
 }
